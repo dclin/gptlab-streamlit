feda17a818743235abf566c78368285f8d5b5bfe
Dave Lin <d2clin@gmail.com>
2023-02-21 01:21:26 -0800

    Terms page - styling

--- app/pages/5_terms.py
+++ app/pages/5_terms.py
@@ -1,5 +1,17 @@
 import streamlit as st 
 
+st.set_page_config(
+    page_title="GPT Lab - Terms",
+    page_icon="https://api.dicebear.com/5.x/bottts-neutral/svg?seed=gptLAb"#,
+    #menu_items={"About": "GPT Lab is a user-friendly app that allows anyone to interact with and create their own AI Assistants powered by OpenAI's GPT-3 language model. Our goal is to make AI accessible and easy to use for everyone, so you can focus on designing your Assistant without worrying about the underlying infrastructure.", "Get help": None, "Report a Bug": None}
+)
+
+st.markdown(
+    "<style>#MainMenu{visibility:hidden;}</style>",
+    unsafe_allow_html=True
+)
+
+
 st.sidebar.markdown('''
 - [Terms of Use](#terms-of-use)
 - [Privacy Policy](#privacy-policy)
